ca5a77bf8455a9cc057c2be9ee5af64b17d923e6
Zack-Bloodshot <zackbloodshot@gmail.com>
2021-07-30 17:18:14 +0530

    fix

--- NotDraken/__main__.py
+++ NotDraken/__main__.py
@@ -67,7 +67,7 @@ async def request(mikey):
   link = None
   keybo = []
   async for message in takemichi.iter_messages(chat, search=query):
-    phto = hek.photo
+    #phto = hek.photo
     txt = message.raw_text.split('|')[0]
     link = message.text.split('(')[1][:-1]
     keybo.append([Button.url(text=txt, url=link)])
